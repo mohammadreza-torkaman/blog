16fb53ab5586d032a1a162d68ea73c0f80ac3625
mohammadreza torkaman <torkaman.mohammadreza@yahoo.com>
2022-05-10 15:31:24 +0430

    DeleteComment Message Implemented

--- x/blog/keeper/msg_server_delete_comment.go
+++ x/blog/keeper/msg_server_delete_comment.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"context"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/mohammadreza-torkaman/blog/x/blog/types"
@@ -9,9 +10,18 @@ import (
 
 func (k msgServer) DeleteComment(goCtx context.Context, msg *types.MsgDeleteComment) (*types.MsgDeleteCommentResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
+	//Get comment to delete
+	commentToDelete, found := k.GetComment(ctx, msg.CommentID)
 
-	// TODO: Handling the message
-	_ = ctx
+	if !found {
+		return nil, sdkerrors.Wrapf(types.ErrID, "Comment doesnt exist")
+	}
+	//If this comment doesn't belong to this post
+	if commentToDelete.PostID != msg.PostID {
+		return nil, sdkerrors.Wrapf(types.ErrID,
+			"Post Blog Id does not exist for which comment with Blog Id %d was made", msg.PostID)
+	}
+	k.RemoveComment(ctx, commentToDelete.Id)
 
 	return &types.MsgDeleteCommentResponse{}, nil
 }
